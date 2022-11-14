6d4241bb65c5d9735544c58e91c4bbd0316ed998
NilanDMeegoda <nilanmeegoda@gmail.com>
2022-11-14 18:47:17 +0530

    few changes in attention

--- src/components/attention_span/attention.tsx
+++ src/components/attention_span/attention.tsx
@@ -1,25 +1,47 @@
-import React from 'react'
+/* eslint-disable react/jsx-key */
+import React, { useEffect, useState } from 'react'
+import { getChild } from '../../services/get_child'
 import ChildData from '../child_data_component/child_data_component'
-import { IChildData } from '../../childtypes'
+import { getAttentionSpanIQScores } from '../../services/getAttentionSpanScores'
+import ReasoningIq from '../reasoning_iq/resoning'
 
 function AttentionScreen () {
-  // const candidateData: IChildData = {
-  //   name: 'nilan',
-  //   institute: 'SLIIT',
-  //   age: 20,
-  //   guardian: 'nandana',
-  //   disorders: 'NA',
-  //   residence: 'Kurunegala'
-  // }
+  const [candidateData, setCandidateData] = useState<any[]>([])
+  const [attentionSpanScores, setAttentionSpanScores] = useState<any[]>([])
+
+  const fetchAttentioonSpanScores = async () => {
+    await setAttentionSpanScores(await getAttentionSpanIQScores())
+  }
+
+  const getCandidateDate = async () => {
+    await setCandidateData(await getChild())
+  }
+  useEffect(() => {
+    void fetchAttentioonSpanScores()
+    void getCandidateDate()
+  }, [])
 
   return (
     <div className="flex flex-col">
   <h2 className="mb-4 text-2xl font-bold"> Attention Span Evaluation</h2>
-  <div className="mt-10 ">
+  {candidateData.length > 0 && (
+    <ChildData
+    firstName={candidateData[0].firstName}
+    institute={candidateData[0].institute}
+    age={candidateData[0].age}
+    parent={candidateData[0].parent}
+    disorders={candidateData[0].disorders}
+    residence={candidateData[0].residence}
+  />
+  )}
+  {attentionSpanScores.length > 0 && (
+    <div className="mt-10 ">
           <div className="overflow-y-auto p-4" style={{ height: '50vh' }}>
-            <div className="mt-10 relative bg-white p-4 rounded-3xl w-full my-4 shadow-xl">
+            {attentionSpanScores.map((item, key) => {
+              return (
+<div key={key} className="mt-10 relative bg-white p-4 rounded-3xl w-full my-4 shadow-xl">
               <div className=" text-white flex items-center absolute rounded-full py-4 px-4 shadow-xl bg-dark-purple left-4 -top-6">
-                Activity 01
+              {item.activityName}
               </div>
               <div className="mt-8">
                 <div>
@@ -69,113 +91,12 @@ function AttentionScreen () {
                 </div>
               </div>
             </div>
-            <div className="mt-10 relative bg-white p-4 rounded-3xl w-full my-4 shadow-xl">
-              <div className=" text-white flex items-center absolute rounded-full py-4 px-4 shadow-xl bg-dark-purple left-4 -top-6">
-                Activity 01
-              </div>
-              <div className="mt-8">
-                <div>
-                <p className="text-xl font-semibold my-2">Level no :</p>
-                <div className="flex space-x-2 text-gray-500 text-sm">
-                  <p>Expected Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Provided Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Score :- </p>
-                </div>
-                <div>
-                <p className="text-xl font-semibold my-2">Level no :</p>
-                <div className="flex space-x-2 text-gray-500 text-sm">
-                  <p>Expected Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Provided Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Score :- </p>
-                </div>
-                </div><div>
-                <p className="text-xl font-semibold my-2">Level no :</p>
-                <div className="flex space-x-2 text-gray-500 text-sm">
-                  <p>Expected Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Provided Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Score :- </p>
-                </div>
-                </div>
-              </div>
-              <div className="border-t-2"></div>
-
-                <div className="flex justify-end">
-                  <div className="my-2">
-                    <p className="font-semibold text-base mb-2">Overall Progress</p>
-                    <div className="text-base text-blue-400 font-semibold text-end">
-                      <p>34%</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="mt-10 relative bg-white p-4 rounded-3xl w-full my-4 shadow-xl">
-              <div className=" text-white flex items-center absolute rounded-full py-4 px-4 shadow-xl bg-dark-purple left-4 -top-6">
-                Activity 01
-              </div>
-              <div className="mt-8">
-                <div>
-                <p className="text-xl font-semibold my-2">Level no :</p>
-                <div className="flex space-x-2 text-gray-500 text-sm">
-                  <p>Expected Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Provided Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Score :- </p>
-                </div>
-                <div>
-                <p className="text-xl font-semibold my-2">Level no :</p>
-                <div className="flex space-x-2 text-gray-500 text-sm">
-                  <p>Expected Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Provided Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Score :- </p>
-                </div>
-                </div><div>
-                <p className="text-xl font-semibold my-2">Level no :</p>
-                <div className="flex space-x-2 text-gray-500 text-sm">
-                  <p>Expected Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Provided Answer :- </p>
-                </div>
-                <div className="flex space-x-2 text-gray-500 text-sm my-3">
-                  <p>Score :- </p>
-                </div>
-                </div>
-              </div>
-              <div className="border-t-2"></div>
-
-                <div className="flex justify-end">
-                  <div className="my-2">
-                    <p className="font-semibold text-base mb-2">Overall Progress</p>
-                    <div className="text-base text-blue-400 font-semibold text-end">
-                      <p>34%</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+              )
+            })}
 
           </div>
       </div>
+  )}
 </div>
   )
 }
---
+++ src/services/getAttentionSpanScores.ts
@@ -0,0 +1,28 @@
+import API from './api'
+
+export const getAttentionSpanIQScores = async () => {
+  console.log('Child id is', localStorage.getItem('candidateID'))
+  const childId = localStorage.getItem('candidateID')
+  const headerConfig = {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS'
+    }
+  }
+  let attentionSpanScores: any
+  let scores: any
+  await API.get('attentionSpanScores/', headerConfig)
+    .then((response) => {
+      attentionSpanScores = response.data
+      scores = attentionSpanScores.filter((obj: { candidateID: string }) => {
+        return obj.candidateID === childId
+      })
+      console.log('AttentionSpan IQ scores', attentionSpanScores)
+      console.log('AttentionSpan IQ scores2', scores)
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+  return scores
+}
